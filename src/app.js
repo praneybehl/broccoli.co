c53e820e7298988449cab04c780bad8b3943dbc0
Praney Behl <praneybehl@gmail.com>
2019-07-28 23:37:37 +1000

    chore(app): add app styles and add InvitePage

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import {ThemeProvider} from 'styled-components';
+import styled, {ThemeProvider} from 'styled-components';
 import theme from './theme';
 import {GlobalStyles} from './theme/global';
 import Header from "./components/Header";
 import ScreenReaderOnly from "./components/ScreenReaderOnly";
+import Footer from "./components/Footer";
+import InvitePage from "./pages/InvitePage";
+
+const AppStyled = styled.div`
+	display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
 
 const App = () => {
 	return (
 		<ThemeProvider theme={theme}>
-			<>
+			<AppStyled>
 				<ScreenReaderOnly href="#mainContent">
 					Skip to main content
 				</ScreenReaderOnly>
 				<Header />
-				<main id="mainContent">
-				</main>
+				<InvitePage id="mainContent" />
+				<Footer/>
 				<GlobalStyles />
-			</>
+			</AppStyled>
 		</ThemeProvider>
 	);
 };
