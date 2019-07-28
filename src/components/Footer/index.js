112a8854d360936cd6880f2991ea7b71c9befe14
Praney Behl <praneybehl@gmail.com>
2019-07-28 22:24:25 +1000

    feat(components): add Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import Container from "../Container";
+
+const FooterStyled = styled.footer`
+	align-items: center;
+	border-top: 2px solid ${({theme}) => theme.colors.grey200};
+	display: flex;
+	padding: 1.6rem 0;
+`;
+
+const FooterText = styled.div`
+	display: block;
+	color: ${({theme}) => theme.colors.grey800};
+	font-size: ${({theme}) => theme.fontSizes[0]};
+	font-weight: ${({theme}) => theme.fontWeights.medium};
+	line-height: 1.3;
+	text-align: center;
+`;
+
+const Footer = (props) => {
+	const copyRightYear = new Date().getFullYear();
+	return (
+		<FooterStyled {...props}>
+			<Container>
+				<FooterText>
+					Made with
+					<span role="img" aria-label="love"> ❤ ️</span>
+					in Melbourne.
+					<br />
+					© {copyRightYear} Broccoli &amp; Co. All rights reserved.
+				</FooterText>
+			</Container>
+		</FooterStyled>
+	)
+};
+
+export default Footer;
