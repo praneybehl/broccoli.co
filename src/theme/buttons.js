19602b149711f95e80ce3cd163ca473be2e83612
Praney Behl <praneybehl@gmail.com>
2019-07-29 22:10:27 +1000

    chore(theme): update button sizes theme

--- src/theme/buttons.js
+++ src/theme/buttons.js
@@ -2,16 +2,16 @@ export const buttonSizes = {
 	small: {
 		height: "42px",
 		fontSize: "1.4rem",
-		padding: "0.9rem 1.7rem"
+		padding: "1.2rem 1.7rem"
 	},
 	default: {
 		height: "48px",
 		fontSize: "1.6rem",
-		padding: "1.2rem 2.4rem"
+		padding: "1.4rem 2.4rem"
 	},
 	large: {
 		height: "54px",
 		fontSize: "1.8rem",
-		padding: "1.5rem 2.8rem"
+		padding: "1.6rem 2.8rem"
 	},
 };
