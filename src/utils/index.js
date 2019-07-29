ee12fadec0a6a6f9a095d0f61e68ae710804b5c6
Praney Behl <praneybehl@gmail.com>
2019-07-30 00:52:20 +1000

    chore(utils): export utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,5 @@
 export {isClient} from './isClient';
+export {validate} from './formValidation';
+export {requestInvitation} from './api';
+export {useForm} from './hooks/useForm';
+export {useModal} from './hooks/useModal';
