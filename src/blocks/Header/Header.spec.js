feacca0de100d2f47909939943ac5e7ca9a66196
Praney Behl <praneybehl@gmail.com>
2019-07-30 22:00:56 +1000

    tests(blocks): update Header spec to use test again defaultProps

--- src/blocks/Header/Header.spec.js
+++ src/blocks/Header/Header.spec.js
@@ -5,19 +5,20 @@ import Header from "./index";
 describe('<Header/> block', () => {
 	afterEach(cleanup);
 
+	const {defaultProps} = Header;
+
 	it('renders a \'header\' HTML element', () => {
 		const { container } = render(<Header />);
 		expect(container.querySelectorAll('header')).toHaveLength(1);
 	});
 
 	it('renders HeaderLogo correctly', () => {
-		const logoText = 'Broccoli & Co.';
 		const { container } = render(<Header />);
-		expect(container.querySelector('a').textContent).toMatch(logoText);
+		expect(container.querySelector('a').textContent).toMatch(defaultProps.logoText);
 	});
 
 	it('renders HeaderLogo link to be \'/\'', () => {
 		const { container } = render(<Header />);
-		expect(container.querySelector('a').href).toMatch('/');
+		expect(container.querySelector('a').href).toMatch(defaultProps.logoHref);
 	});
 });
